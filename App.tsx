c71a314a040e572768042d5ab94fceb73ddda260
Chia02222 <wanjun123@1utar.my>
2025-07-06 19:59:06 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -353,8 +353,8 @@ const DetailsAndRegistration: React.FC = () => {
                             <div className="bg-gradient-to-br from-fuchsia-500 via-purple-600 to-indigo-500 p-1 rounded-[1.25rem] shadow-lg">
                                 <div className="bg-slate-900 rounded-2xl p-8 text-center">
                                     <div className="mt-6 flex items-baseline justify-center gap-x-3">
-                                        <span className="text-6xl sm:text-4xl font-extrabold bg-gradient-to-r from-fuchsia-400 to-pink-500 text-transparent bg-clip-text">RM599</span>
-                                        <span className="text-3xl sm:text-xl font-medium text-slate-500 line-through">RM899</span>
+                                        <span className="text-2xl sm:text-4xl font-extrabold bg-gradient-to-r from-fuchsia-400 to-pink-500 text-transparent bg-clip-text">RM599</span>
+                                        <span className="text-xl sm:text-xl font-medium text-slate-500 line-through">RM899</span>
                                     </div>
                                     <p className="mt-4 text-slate-400">Offer valid until July 15</p>
                                 </div>
